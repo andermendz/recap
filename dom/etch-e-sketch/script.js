b4e028f038ee70efbb3004126d05826779d2eb59
andermendz <andersonmendz19@gmail.com>
2025-01-03 19:34:19 -0500

    improved mouseover and events

--- dom/etch-e-sketch/script.js
+++ dom/etch-e-sketch/script.js
@@ -1,4 +1,5 @@
 let sketchGrid = document.querySelector("#sketch-grid");
+let button = document.querySelectorAll('button')
 
 var gridProportions = 20;
 var proportionsRatio = gridProportions * gridProportions;
@@ -9,15 +10,38 @@ sketchGrid.style.gridTemplateRows = `repeat(${gridProportions}, 1fr)`;
 for (let i = 0; i <= proportionsRatio; i++) {
   let square = document.createElement("div");
   square.classList.add("square");
-  square.id = `square-${i}`
+  square.id = `square-${i}`;
   sketchGrid.appendChild(square);
 
   var squares = document.querySelectorAll(".square");
 }
 
-squares.forEach((square) => {
-  console.log("asas");
-  square.onclick = (e) => {
-    console.log("s");
-  };
+let isMouseDown = false;
+
+document.addEventListener("mousedown", (e) => {
+    e.preventDefault()
+  isMouseDown = true;
+  
 });
+
+document.addEventListener("mouseup", () => {
+    isMouseDown = false;
+  });
+
+squares.forEach((square) => {
+  square.addEventListener("mousemove", (e) => {
+
+
+    if (isMouseDown) {
+      
+      console.log(square.id);
+    }
+  });
+});
+
+
+button.forEach((btn) => {
+    btn.onclick = () =>{
+        console.log('click click')
+    }
+})
