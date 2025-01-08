02069e7aee708f1cae58e71a54bcf271767c8ec8
andermendz <andersonmendz19@gmail.com>
2025-01-07 20:55:45 -0500

    created render function

--- dom/etch-e-sketch/script.js
+++ dom/etch-e-sketch/script.js
@@ -1,35 +1,38 @@
-let sketchGrid = document.querySelector("#sketch-grid");
+let sketchGrid = document.getElementById("sketch-grid");
 let button = document.querySelectorAll("button");
 
-var gridProportions = 30;
-var proportionsRatio = gridProportions * gridProportions;
+let gridTen = document.getElementById("10");
+let gridTwent = document.getElementById("20");
+let gridThird = document.getElementById("30");
 
-sketchGrid.style.gridTemplateColumns = `repeat(${gridProportions}, 1fr)`;
-sketchGrid.style.gridTemplateRows = `repeat(${gridProportions}, 1fr)`;
+renderGrid(30);
 
-for (let i = 0; i <= proportionsRatio; i++) {
-  let square = document.createElement("div");
-  square.classList.add("square");
-  square.id = `square-${i}`;
-  sketchGrid.appendChild(square);
+function renderGrid(proportions) {
+  sketchGrid.innerHTML = "";
+  var gridProportions = proportions;
+  var proportionsRatio = gridProportions * gridProportions;
 
-
+  for (let i = 0; i <= proportionsRatio; i++) {
+    let square = document.createElement("div");
+    square.classList.add("square");
+    square.id = `square-${i}`;
+    sketchGrid.appendChild(square);
+  }
+  sketchGrid.style.gridTemplateColumns = `repeat(${gridProportions}, 1fr)`;
+  sketchGrid.style.gridTemplateRows = `repeat(${gridProportions}, 1fr)`;
 }
 
- 
 let squares = document.querySelectorAll(".square");
 squares.forEach((square) => {
   square.addEventListener("mousemove", (e) => {
     if (isMouseDown) {
       console.log(square.id);
-      square.style.backgroundColor = '#0000';
+      square.style.backgroundColor = "#0000";
     }
   });
 });
 
 let isMouseDown = false;
-
-
 
 document.addEventListener("mousedown", (e) => {
   e.preventDefault();
@@ -42,8 +45,6 @@ document.addEventListener("mouseup", () => {
   isMouseDown = false;
 });
 
-
-
 button.forEach((btn) => {
   btn.onclick = () => {
     console.log("click click");
@@ -51,22 +52,15 @@ button.forEach((btn) => {
 });
 
 
-let gridTen = document.getElementById('grid10-10')
 
 gridTen.onclick = () => {
-  var gridProportions = 10;
-  var proportionsRatio = gridProportions * gridProportions;
-
-sketchGrid.style.gridTemplateColumns = `repeat(${gridProportions}, 1fr)`;
-sketchGrid.style.gridTemplateRows = `repeat(${gridProportions}, 1fr)`;
-}
-
-let gridTwent = document.getElementById('grid20-20')
+  renderGrid(10)
+};
 
 gridTwent.onclick = () => {
-  var gridProportions = 20;
-  var proportionsRatio = gridProportions * gridProportions;
+  renderGrid(20)
+};
 
-sketchGrid.style.gridTemplateColumns = `repeat(${gridProportions}, 1fr)`;
-sketchGrid.style.gridTemplateRows = `repeat(${gridProportions}, 1fr)`;
-}
+gridThird.onclick = () => {
+  renderGrid(30)
+};
